dd06f162c86d8dbfaa703779e9c1de6e773af5a5
wushauang <2057197427@qq.com>
2019-03-06 16:25:00 +0800

    第一次提交

---
+++ testmaven/src/main/java/com/ys/App.java
@@ -0,0 +1,19 @@
+package com.ys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a = 1;
+        if(a>5){
+            System.out.println("a>5");
+        }
+        System.out.println("a<5");
+
+    }
+}
